d3c1e3aedc29edcb5d37eedddc6c58f474bbb7ec
leblanc3 <renierdelacruz666@gmail.com>
2025-10-05 01:39:42 +0800

    a

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -10,17 +10,5 @@ def index():
 def profile():
     return render_template('profile.html')
 
-@app.route('/works', methods=['GET', 'POST'])
-def works():
-    result = None
-    if request.method == 'POST':
-        input_string = request.form.get('inputString', '')
-        result = input_string.upper()
-    return render_template('touppercase.html', result=result)
-
-@app.route('/contact')
-def contact():
-    return "Contact Page. please create me an html page with dummy contact info"
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     app.run(debug=True)
